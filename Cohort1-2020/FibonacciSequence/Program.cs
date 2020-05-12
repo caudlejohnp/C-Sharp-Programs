b918b3de6f53a8a047c1d468edd3a9bde31f92a9
caudlejohnp <caudlejohnp@gmail.com>
2020-05-11 19:45:26 -0500

    added book DB and screenshots

---
+++ Cohort1-2020/FibonacciSequence/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FibonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j = 2;
+            int h = 0;
+            int temp = 0;
+
+            h = i + j;
+
+            List<int> myList = new List<int> { };
+            
+        }
+    }
+}
